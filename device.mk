491ee097aa6b79843e3a6849f1cf2b990a1d0a5f
Nikhil Bhandare <40919273+linux-xd@users.noreply.github.com>
2023-10-07 22:19:19 +0530

    Add reboot_fastboot action to power menu

--- device.mk
+++ device.mk
@@ -20,6 +20,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+
+
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Additional native libraries
