20b3cb6e0622774b81325646b0105eaca7c7f7b8
Nikhil Bhandare <40919273+linux-xd@users.noreply.github.com>
2023-11-19 13:34:48 +0530

    X2: using gapps from derp 14 branch

--- device.mk
+++ device.mk
@@ -511,7 +511,7 @@ PRODUCT_BOOT_JARS += \
 $(call inherit-product-if-exists, vendor/realme/X2/X2-vendor.mk)
 
 # Google Basic apps
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
 
 #Inherit Realme Camera
 # $(call inherit-product-if-exists, vendor/camera/X2/realmecamera.mk)
