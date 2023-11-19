1a84c3f10d7d928ca02308feaef3ac2afde64d6c
Nikhil Bhandare <40919273+linux-xd@users.noreply.github.com>
2023-11-19 16:03:43 +0530

    X2: remove_packages: remove unwanted packages

--- device.mk
+++ device.mk
@@ -389,6 +389,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
+# Remove Unwanted Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
