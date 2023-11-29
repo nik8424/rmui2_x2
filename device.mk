d7ae21ccdbdb046f46e8b029f59921799bd1a621
Nikhil Bhandare <40919273+linux-xd@users.noreply.github.com>
2023-11-29 10:39:10 +0530

    X2:Build 'android.hardware.media.omx@1.0-service' OMX is no longer built by default for 64bit-only devices

--- device.mk
+++ device.mk
@@ -294,6 +294,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service \
     libc2dcolorconvert \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
