e9d5652e0b54dd60945a7a6a9d87a537beffb566
Nikhil Bhandare <40919273+linux-xd@users.noreply.github.com>
2024-01-16 19:07:56 +0530

    X2: Dolby: Link dolby blobs with v33 libstagefright_foundation

--- extract-files.sh
+++ extract-files.sh
@@ -29,6 +29,11 @@ function blob_fixup() {
         system_ext/lib64/lib-imsvideocodec.so)
             "${PATCHELF}" --add-needed "lib-imsvtshim.so" "${2}"
             ;;
+        vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefright_soft_ac4dec.so | \
+        vendor/lib/libstagefrightdolby.so | vendor/lib64/libstagefright_soft_ddpdec.so | \
+        vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefrightdolby.so)
+            ${PATCHELF} --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
