f40e72d57d15ffb7ab4c49f54fd70c280060cf6e
Nikhil Bhandare <40919273+linux-xd@users.noreply.github.com>
2024-02-01 18:06:48 +0530

    X2: Build libprotobuf-cpp-full-3.9.1-vendorcompat

--- device.mk
+++ device.mk
@@ -429,7 +429,8 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2 \
     libprotobuf-cpp-full \
-    libprotobuf-cpp-lite-3.9.1-vendorcompat
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
 
 # Rootdir
 PRODUCT_COPY_FILES += \
